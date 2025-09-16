7ce69e289f10af1b4cac578219a14da0c48d4dde
Simon Olsson <scmolsson@pm.me>
2025-09-16 14:32:22 +0200

    add fetchAllBookings to fetch all bookings from dynamoDB. Returns an unmarshalled object + throws error

---
+++ services/fetchAllBookings.mjs
@@ -0,0 +1,20 @@
+import { client } from "./db.mjs";
+import { QueryCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+
+export const fetchBookings = async () => {
+  try {
+    const params = new QueryCommand({
+      TableName: "bonzAiTable",
+      KeyConditionExpression: "pk = :pk",
+      ExpressionAttributeValues: {
+        ":pk": { S: "BOOKING" },
+      },
+    });
+
+    const result = await client.send(params);
+    return result.Items.map(unmarshall);
+  } catch (error) {
+    return console.error("Fetch Error:", error.message);
+  }
+};
