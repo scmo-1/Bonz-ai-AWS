b46bfbb928e3c793cb916fea572f994b77bb69ee
Oskar Blomberg <blomberg.oskar@gmail.com>
2025-09-16 19:55:24 +0200

    utils/

--- utils/isRoomCapEnough.mjs
+++ utils/isRoomCapEnough.mjs
@@ -2,6 +2,6 @@ export const isRoomCapEnough = (
   guests,
   { single = 0, double = 0, suite = 0 }
 ) => {
-  const cap = parseInt(single) + parseInt(double) * 2 + parseInt(suite) * 3;
+  const cap = single + double * 2 + suite * 3;
   return parseInt(guests) <= cap;
 };
